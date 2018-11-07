8bf25fcc4f4c55bb2c874a3330e2e193c35363f0
Patrick <patrick.lopdrup@gmail.com>
2018-11-07 07:12:46 +0100

    lavet så man tæller spillerturen op med en static counter, som tæller op hver gang man laver et Spiller objekt

--- src/Spiller.java
+++ src/Spiller.java
@@ -2,16 +2,24 @@ public class Spiller {
     private final int STARTSALDO = 1000;
     private int saldo;
     private String navn;
+    private static int counter = 0; //tæller op hver gang en spiller bliver lavet
+    private int spillerTur = 0;
 
     public Spiller(String Navn) {
         saldo = STARTSALDO;
         navn = Navn;
         //hvis man lavede en static counter, som man talte op hver gang man lavede en spiller fra constuctoren?
         //ville måske virke
+        spillerTur = counter; //sætter spillerTuren til antal spiller der er blevet lavet
+        counter++;
     }
 
     public String getNavn() {
         return this.navn;
+    }
+
+    public int getSpillerTur() {
+        return spillerTur; //måske skal man returne counter-1 i stedet for, da den er static?
     }
 
     public int getSaldo() {
